45715efb96e33fe2d74faa37bc404deacf88e55e
Soubhagya Swain <soubhagyaswain141@gmail.com>
2026-03-16 23:20:24 +0530

    Restructuring

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,3 @@ fn main() -> eframe::Result<()> {
         Box::new(|_cc| Ok(Box::new(MyApp::default()))),
     )
 }
-
-#[derive(Default)]
-struct MyApp;
-
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello, egui!");
-        });
-    }
-}
